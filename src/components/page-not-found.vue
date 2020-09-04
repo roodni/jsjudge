672521e391794e81c3d535d0f1d255f52c075f40
roodni <roodni@users.noreply.github.com>
2020-09-04 13:09:32 +0000

    deploy: 66479356e3a24f97e51d6d150f26cc5a3afe1a0b

--- src/components/page-not-found.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <v-card>
-    <v-card-title>ページが存在しない</v-card-title>
-    <v-card-text>無</v-card-text>
-  </v-card>
-</template>
--- src/utils/set-title.js
+++
@@ -1,8 +0,0 @@
-export default function setTitle(title) {
-  const app_name = 'JSJudge';
-  if (title === '') {
-    document.title = app_name;
-  } else {
-    document.title = `${title} | ${app_name}`;
-  }
-}
