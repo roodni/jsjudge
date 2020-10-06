55dae7e9e55a8459bfce5ea4b6cc0424a8707860
rodoni <41773436+roodni@users.noreply.github.com>
2020-10-06 18:33:37 +0900

    問題追加

---
+++ src/assets/problems/string2num.js
@@ -0,0 +1,38 @@
+import md from './string2num.md';
+
+export default {
+  markdown: md,
+  args_default: 'str',
+  testcases: [
+    {
+      name: '-100〜100',
+      judge: (f, ac, wa) => {
+        for (let i = -100; i <= 100; i++) {
+          const s = i.toString();
+          if (f(s) !== i) {
+            wa();
+            return;
+          }
+        }
+        ac();
+      }
+    },
+    {
+      name: 'その他',
+      judge: (f, ac, wa) => {
+        const ff = s => {
+          const n = parseInt(s, 10);
+          return isNaN(n) ? 0 : n;
+        };
+        const l = ['12345', '.1', '0xff', '1.2', 'Infinity'];
+        for (const s of l) {
+          if (f(s) !== ff(s)) {
+            wa();
+            return;
+          }
+        }
+        ac();
+      }
+    }
+  ]
+};
